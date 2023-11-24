edc96a019e749b84466830ec5bc67719de784372
JulianSC <julian.sc@knownonline.com>
2023-11-24 18:40:13 -0300

    agregada la pagina de vista de producto general

---
+++ app/productos/[...slug]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { Fragment, useEffect, useState } from "react";
+export default function Producto() {
+  return (
+    <main className="PageMainContainer px-4 py-12 xl:px-16">
+      <div>
+        Producto
+      </div>
+    </main>
+  );
+}
