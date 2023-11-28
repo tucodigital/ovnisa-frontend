1b54dc0d3418ae65d6d7839dceab34a14b3b0af2
JulianSC <julian.sc@knownonline.com>
2023-11-28 17:36:07 -0300

    creada la consulta de la pagina de producto

--- app/productos/[...slug]/page.tsx
+++ app/productos/[...slug]/page.tsx
@@ -1,7 +1,36 @@
 "use client";
 
 import React, { Fragment, useEffect, useState } from "react";
-export default function Producto() {
+import { fetchAPI } from "@/lib/api";
+
+export default function Producto(context) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getProductos();
+  }, [context.params.slug]);
+
+  const getProductos = async () => {
+    setLoading(true);
+    try {
+      const productRes = await fetchAPI(`/productos/${context.params.slug}`, {
+        populate: {
+          imagen_principal: "*",
+          rubros: "*",
+          marca: "*",
+          tipos_de_productos: "*",
+          keywords: "*",
+          categorias: "*",
+        },
+      });
+      console.log(productRes);
+      setLoading(false);
+    } catch (e: any) {
+      console.error(e.response);
+      setLoading(false);
+    }
+  };
+
   return (
     <main className="PageMainContainer px-4 py-12 xl:px-16">
       <div>
