f3990213fc1e3818d62f799cd0e74a9fae3d4ede
JulianSC <julian.sc@knownonline.com>
2023-12-14 00:11:17 -0300

    Fixes estilos skeletons mobile

--- components/MainMenu/MainMenuSkeletons.tsx
+++ components/MainMenu/MainMenuSkeletons.tsx
@@ -1,12 +1,12 @@
 export const ContactMainMenuItemDesktopSkeleton = ({ width = 32 }) => {
   return (
     <div className="flex flex-row gap-2 items-center">
-      <div className="animate-pulse w-6 rounded-full bg-slate-200 h-6 " />
-      <div className={`animate-pulse w-${width} rounded-md bg-slate-200 h-4`} />
+      <div className="animate-pulse w-7 rounded-full bg-blue-800 h-7 " />
+      <div className={`animate-pulse w-${width} rounded bg-blue-800 h-7`} />
     </div>
   );
 };
 
 export const NavigationMainMenuItemDesktopSkeleton = () => (
-  <div className="animate-pulse rounded-md bg-slate-200 h-4 w-28" />
+  <div className="animate-pulse rounded-md bg-blue-800 h-7 w-28" />
 );
