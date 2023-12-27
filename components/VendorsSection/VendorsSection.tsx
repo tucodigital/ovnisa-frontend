18c903e9abfa88f8c26f352d9c2f0d1e8d8de1b0
nacchogonza <nachomgonzalez93@gmail.com>
2023-12-27 17:54:05 -0300

    Fixes VendorsSection responsive

--- components/VendorsSection/VendorsSection.tsx
+++ components/VendorsSection/VendorsSection.tsx
@@ -9,29 +9,31 @@ export const VendorsSection = ({ title, vendors }) => {
   return (
     <div className="bg-white pt-8 pb-12 lg:pb-28 px-10 lg:px-28 flex gap-4 lg:gap-10 flex-col lg:flex-row w-full ">
       {/* Título de la página que se muestra solo en Mobile */}
-      <h2 className="block lg:hidden text-ov-primaryLight font-bold text-2xl mb-4">
+      <h2 className="block md:hidden text-ov-primaryLight font-bold text-2xl mb-4">
         {title}
       </h2>
-      <div className="grid lg:grid-cols-4 gap-8 lg:gap-4 w-full">
+      <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 lg:gap-4 w-full">
         {vendors.map((vendor) => (
-          <div className="rounded-lg lg:bg-gray-100 lg:p-4 relative lg:hover:shadow-lg lg:transition lg:duration-300 flex flex-row gap-4 align-items-center">
+          <div className="rounded-lg md:bg-gray-100 md:p-4 relative md:hover:shadow-lg md:transition md:duration-300 flex flex-row gap-4 align-items-center">
             <Image
-              alt={"image_desktop?.data?.attributes?.alternativeText"}
-              src={`https://placehold.co/400x400/orange/white`}
-              width={150}
-              height={150}
+              alt={vendor?.avatar?.data?.attributes?.alternativeText}
+              src={vendor?.avatar?.data?.attributes?.url}
+              width={100}
+              height={100}
+              objectFit="fill"
               loader={loaderProp}
-              className="hidden lg:block"
+              className="hidden md:block w-32 h-32"
             />
 
-            <div>
+            <div className="flex items-center">
               <Image
-                alt={"image_desktop?.data?.attributes?.alternativeText"}
-                src={`https://placehold.co/400x400/orange/white`}
+                alt={vendor?.avatar?.data?.attributes?.alternativeText}
+                src={vendor?.avatar?.data?.attributes?.url}
                 width={100}
                 height={100}
+                objectFit="fill"
                 loader={loaderProp}
-                className="block lg:hidden rounded-full border border-1 border-gray-700"
+                className="block md:hidden rounded-full border border-1 border-gray-700 w-32 h-32"
               />
             </div>
             <div className="flex flex-col lg:gap-1 justify-center">
