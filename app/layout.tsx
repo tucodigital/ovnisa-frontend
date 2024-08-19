b510e7bbdba60af3b0d3dc1bf19f1ed0a09378fb
JulianSC <julian.sc@knownonline.com>
2024-08-19 12:19:06 -0300

    Agregado cliengo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 import "./globals.css";
 import "./index.css";
 import { MainMenu } from "@/components/MainMenu/MainMenu";
+import Script from "next/script";
 
 const Footer = dynamic(() =>
   import("@/components/Footer/Footer").then((module) => module.Footer)
@@ -19,6 +20,7 @@ export default function RootLayout({
 
   return (
     <html lang="es-AR">
+      {/* <script type="text/javascript">(function () { var ldk = document.createElement('script'); ldk.type = 'text/javascript'; ldk.async = true; ldk.src = 'https://s.cliengo.com/weboptimizer/5bbcb1c2e4b036ee4dde5680/5bbcb1c3e4b036ee4dde5683.js?platform=view_installation_code'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ldk, s); })();</script> */}
       <head>
         <title>Ovnisa</title>
         <meta
@@ -30,6 +32,10 @@ export default function RootLayout({
         <meta
           property="og:description"
           content="Soluciones técnicas al servicio de la industria"
+        />
+        <Script
+          src=" https://s.cliengo.com/weboptimizer/5bbcb1c2e4b036ee4dde5680/5bbcb1c3e4b036ee4dde5683.js?platform=view_installation_code"
+          strategy="lazyOnload"
         />
       </head>
       <body>
