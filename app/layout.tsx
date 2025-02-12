85b49b2db4b1241d1c7acc90d283c9a92d708809
tulindor <julian.sc@knownonline.com>
2025-02-12 10:17:05 -0300

    upload google tag ovnisa

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,8 +55,27 @@ export default function RootLayout({
           src=" https://s.cliengo.com/weboptimizer/5bbcb1c2e4b036ee4dde5680/5bbcb1c3e4b036ee4dde5683.js?platform=view_installation_code"
           strategy="lazyOnload"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KNKXLDZ');
+            `,
+          }}
+        />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNKXLDZ"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <MainMenu
           showSearchOverlay={showSearchOverlay}
           setShowSearchOverlay={setShowSearchOverlay}
