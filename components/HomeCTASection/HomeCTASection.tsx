22214328bcc6d4e99f405e85d43acd8e22b43416
tulindor <tulisc@gmail.com>
2025-11-22 15:19:29 -0300

    Ajustes slider de marca Ajuste CTA home

--- components/HomeCTASection/HomeCTASection.tsx
+++ components/HomeCTASection/HomeCTASection.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import Image from "next/image";
 import { loaderProp } from "@/lib/utils";
 import { HomeSectionContent } from "@/types/components/HomeCtaSectionTypes";
+import ReactMarkdown from "react-markdown";
 
-export const HomeCTASection = ({ title }: HomeSectionContent) => {
+export const HomeCTASection = ({ title, subtitle }: HomeSectionContent) => {
   return (
     <div className="bg-ov-primary py-10 lg:py-20">
       <div className="PageMainContainer px-4">
-        {title ? (
-          <h3 className="text-white text-3xl font-bold mb-8">{title}</h3>
-        ) : null}
+        <div className="grid grid-cols-1 lg:grid-cols-6 gap-4">
+          <div className="lg:col-span-4">
+            {title ? (
+              <h3 className="text-white text-3xl font-bold mb-8">{title}</h3>
+            ) : null}
+
+            {subtitle ? (
+              <ReactMarkdown
+                className="font-light prose prose-h2:font-light text-lg xl:text-xl text-left text-white mb-2"
+                children={subtitle}
+              />
+            ) : null}
+          </div>
+          <div className="lg:col-span-2"></div>
+        </div>
       </div>
     </div>
   );
--- types/components/BrandsSectionTypes.ts
+++ types/components/BrandsSectionTypes.ts
@@ -1,4 +1,5 @@
 export interface BrandsSectionContent {
+  id:number;
   title: String;
   brands: BrandContent[];
 }
