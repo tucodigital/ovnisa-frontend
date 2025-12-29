de49949f7e8af1f3575dae19e5a4a05f7bc08cf3
tulindor <tulisc@gmail.com>
2025-12-29 10:16:52 -0300

    Ajuste de grosor de lupa en el header

--- components/MainMenu/MainMenu.tsx
+++ components/MainMenu/MainMenu.tsx
@@ -191,7 +191,7 @@ export const MainMenu = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth={4}
+                  strokeWidth={3}
                   stroke="currentColor"
                   className="w-8 h-8"
                 >
