c7647be451e09a07dffac01d4d3fd360ec2ccceb
nacchogonza <55638343+nacchogonza@users.noreply.github.com>
2026-03-13 10:38:52 -0300

    Se ajusta logica de TagMarca para unico tag

--- components/products/TagMarca.tsx
+++ components/products/TagMarca.tsx
@@ -3,38 +3,11 @@
 import { TagMarcaContent } from "@/types/productoTypes";
 
 export const TagMarca = ({ marca }: TagMarcaContent) => {
-  switch (marca) {
-    case "Ovniflex":
-      return (
-        <div className="absolute top-3 right-3 z-20">
-          <div className="bg-ov-primaryLight rounded text-white px-3 py-1">
-            <h4 className="font-medium uppercase text-[10px]">{marca}</h4>
-          </div>
-        </div>
-      );
-    case "Mima":
-      return (
-        <div className="absolute top-3 right-3 z-20">
-          <div className="bg-red-600 rounded text-white px-3 py-1">
-            <h4 className="font-medium uppercase text-[10px]">{marca}</h4>
-          </div>
-        </div>
-      );
-    case "Mr. Bond":
-      return (
-        <div className="absolute top-3 right-3 z-20">
-          <div className="bg-cyan-600 rounded text-white px-3 py-1">
-            <h4 className="font-medium uppercase text-[10px]">{marca}</h4>
-          </div>
-        </div>
-      );
-    default:
-      return (
-        <div className="absolute top-3 right-3 z-20">
-          <div className="bg-gray-200 rounded text-gray-700 px-3 py-1">
-            <h4 className="font-medium uppercase text-[10px]">{marca}</h4>
-          </div>
-        </div>
-      );
-  }
+  return (
+    <div className="absolute top-3 right-3 z-20">
+      <div className="bg-ov-primaryLight rounded text-white px-3 py-1">
+        <h4 className="font-medium uppercase text-[10px]">{marca}</h4>
+      </div>
+    </div>
+  );
 };
